bccb3dd5592899e798721d24e8dda3f53ae83983
Kader Mohideen Fasid <fasidmpm@gmail.com>
2019-07-22 00:08:21 +0530

    Next Module, Stats. So far with Tuples, CSV Read, Enums

---
+++ stats/build/CsvReader.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var CsvReader = /** @class */ (function () {
+    function CsvReader() {
+    }
+    return CsvReader;
+}());
+exports.CsvReader = CsvReader;
---
+++ stats/src/CsvFileReader.ts
@@ -0,0 +1,32 @@
+import fs from "fs";
+import {dateStringToDate} from "./utils";
+import {MatchResult} from "./MatchResult";
+
+export type MatchData = [Date, string, string, number, number, MatchResult, string];
+
+export class CsvFileReader {
+  data: MatchData[] = [];
+
+  constructor(public filename: string) {}
+
+  read(): void {
+    this.data = fs.readFileSync(this.filename, {
+      encoding: 'utf-8'
+    })
+      .split('\n')
+      .map(
+        (row: string): string[] => row.split(',')
+      ).map(
+        (row: string[]): MatchData =>
+          [
+            dateStringToDate(row[0]),
+            row[1],
+            row[2],
+            parseInt(row[3]),
+            parseInt(row[4]),
+            row[5] as MatchResult, //Known as Type assertion
+            row[6]
+          ]
+      );
+  }
+}
