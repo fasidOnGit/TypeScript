1259d3312c4e383c023e0aac451f17b4656df444
Kader Mohideen Fasid <fasidmpm@gmail.com>
2019-07-28 19:37:29 +0530

    More on Generics

--- Features/generics.ts
+++ Features/generics.ts
@@ -49,3 +49,84 @@ holdAsNumber.data = 2;
 const holdAsString = new HoldAnything<string>();
 holdAsString.data = 'string';
 
+
+
+class ArrayOfNumbers {
+  constructor(public collection: number[]) {}
+
+  get(index: number): number {
+    return this.collection[index];
+  }
+}
+
+
+class ArrayOfStrings {
+  constructor(public collection: string[]) {}
+
+  get(index: number): string {
+    return this.collection[index];
+  }
+}
+
+class ArrayOfAnything<T> {
+  constructor(public collection: T[]) {}
+
+  get(index: number): T {
+    return this.collection[index];
+  }
+}
+
+new ArrayOfAnything<string>(['a', 'b']);
+
+//Type interference with Generics
+const arr = new ArrayOfAnything(['ab','c']); //Even though, we didn't assign the type like above. yet typescript automatically figure out the type of the generics that we passed in.
+
+
+// Generics around Functions
+
+function printStrings(arr: string[]): void {
+  arr.forEach(console.log);
+}
+function printNumbers(arr: number[]): void {
+  arr.forEach(console.log);
+}
+
+function printAnything<T>(arr: T[]): void {
+  arr.forEach(console.log);
+}
+//without annotations
+const printNumbres = printAnything([1,2,3]);
+const printStringss = printAnything(['1','2','3']);
+
+//with annotation
+const printNum = printAnything<number>([1,2,3]);
+
+//use with annotation : recommended.
+
+
+//Generics Constraints
+
+class Car {
+  print() {
+    console.log('I am a car');
+  }
+}
+
+
+class House {
+  print() {
+    console.log('I am a house');
+  }
+}
+
+function printHouseOrCars<T>(arr: T[]): void {
+  arr.forEach(each => each.print()); // TS throws error!!1  Gotcha!! calling print method.
+}
+
+//Fix
+interface Printable {
+  print(): void;
+}
+function printHoursesOrCars<T extends Printable>(arr: T[]): void {
+  arr.forEach(each => each.print())  // No error anymore.  so we can't call printHousesOrCars([1,2,3])
+}
