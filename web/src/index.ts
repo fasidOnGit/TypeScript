b91a6cf856e31e6b6010f4eba2a699a42e52293a
Kader Fasid <fasidmpm@gmai.com>
2020-01-05 20:29:46 +0530

    Added events handling and triggeing

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,1 +1,18 @@
-console.log('hello parcel, go to hell');
+import {User} from './models/User';
+
+const user = new User({name: 'Kader', age: 20});
+
+user.on('custom', () => {
+  console.log('Change #1');
+});
+user.on('custom', () => {
+  console.log('Change #2');
+});
+user.on('save', () => {
+  console.log('Save was triggered');
+});
+
+console.log(user);
+
+user.trigger('custom');
+user.trigger('save');
--- web/src/models/User.ts
+++ web/src/models/User.ts
@@ -1,5 +1,44 @@
+interface UserProps {
+  name: string;
+  age: number;
+}
 export class User {
-  constructor(
-    private data: {name: string; age: number}
-    ) {}
+  /**
+   * Adding events
+   */
+  events: {[key: string]: CallableFunction[]} = {};
+
+  constructor(private data: Partial<UserProps>) {}
+
+  get(propName: keyof UserProps): (string | number | undefined) {
+    return this.data[propName];
+  }
+
+  set(update: Partial<UserProps>): void {
+    Object.assign(
+      this.data,
+      update
+    );
+  }
+
+  /**
+   * Registering events with callbacks.
+   * @param eventName
+   * @param callback
+   */
+  on(eventName: string, callback: CallableFunction): void {
+    const handlers = this.events[eventName] || [];
+    handlers.push(callback);
+    this.events[eventName] = handlers;
+  }
+
+  trigger(eventName: string): void {
+    const handlers = this.events[eventName];
+    if (!handlers || handlers.length === 0) {
+      return;
+    }
+    handlers.forEach(callback => {
+      callback();
+    });
+  }
 }
